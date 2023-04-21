5a0a7ae1ae9e828417ab09376833cef57c0a8c97
seyithan.topal <18354002+seyithantopal@users.noreply.github.com>
2023-04-21 22:31:23 +0200

    move icons into icons folder

---
+++ src/components/Icons/HamburgerIcon.jsx
@@ -0,0 +1,16 @@
+export const HamburgerIcon = ({ className, onClick }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      width={24}
+      height={24}
+      className={className}
+      onClick={onClick}
+    >
+      <path d="M3 4H21V6H3V4ZM3 11H21V13H3V11ZM3 18H21V20H3V18Z"></path>
+    </svg>
+  );
+};
+
+export default HamburgerIcon;
